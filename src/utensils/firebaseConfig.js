a41762d296f126a0a3d8032ac228012914902d34
ReactNativeAmalgum <reactnativeamalgum@gmail.com>
2024-05-30 23:34:30 +0530

    firebase removed

--- src/utensils/firebaseConfig.js
+++ src/utensils/firebaseConfig.js
@@ -4,13 +4,7 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCe6MYZJogGWAQoyPbltCb82FN1dswSwsc",
-  authDomain: "todo-f24ae.firebaseapp.com",
-  projectId: "todo-f24ae",
-  storageBucket: "todo-f24ae.appspot.com",
-  messagingSenderId: "77038806724",
-  appId: "1:77038806724:web:c3a66c5b95bcedac9f8b91",
-  measurementId: "G-ZF8HF7L573"
+
 };
 
 // Initialize Firebase
